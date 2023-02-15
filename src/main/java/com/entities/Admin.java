dfe9cdf7052c177bb61edace997e34d998b3e583
MalCaor <xav.lemen@orange.fr>
2023-02-15 12:09:54 +0100

    bdd link

--- src/main/java/com/entities/Admin.java
+++ src/main/java/com/entities/Admin.java
@@ -5,9 +5,11 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Table;
 
 @Entity
 @Data
+@Table(name="t_Admin_adm")
 public class Admin {
     @javax.persistence.Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
